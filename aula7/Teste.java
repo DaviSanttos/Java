9c99c5d6078d2c43c7f6d91b8d545b949f2773bc
DaviSanttos <102439735+DaviSanttos@users.noreply.github.com>
2025-10-09 21:15:40 -0300

    Update Teste.java

--- aula7/Teste.java
+++ aula7/Teste.java
@@ -1,43 +1,50 @@
-class FiguraGeometrica {
-    public double PI = 3.14159;
+package test;
 
-    public static void saudacao(){
-        System.out.println("Iai meu jovem");
-    }
-
-    public double calcularArea(double paramUnico){
-        return 0.0;
-    }
-
-    public double calcularArea(double base, double altura){
-        return 0.0;
+abstract class FiguraGeometrica {
+    public abstract double calcularArea();
+    
+    public static void saudacao() {
+        System.out.println("Olá, sou uma figura geométrica!");
     }
 }
-
 
 class Circulo extends FiguraGeometrica {
+    private double raio;
+    private static final double PI = 3.14159;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
     @Override
-    public double calcularArea(double raio) {
-        return this.PI*(raio*2);
+    public double calcularArea() {
+        return PI * raio * raio;
     }
 }
 
 class Retangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
     @Override
-    public double calcularArea(double largura, double altura) {
-        return this.PI*(raio*2);
+    public double calcularArea() {
+        return base * altura;
     }
 }
-
-class Retangulo extends FiguraGeometrica {
-    @Override
-    public double calcularArea(double base, double altura) {
-        return (base*altura)/2;
-    }
-}
-
-
 
 public class Teste {
-    
+    public static void main(String[] args) {
+        FiguraGeometrica.saudacao();
+
+        Circulo c = new Circulo(5);
+        Retangulo r = new Retangulo(4, 6);
+
+        System.out.println("Área do círculo: " + c.calcularArea());
+        System.out.println("Área do retângulo: " + r.calcularArea());
+    }
 }
